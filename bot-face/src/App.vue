a3c88b55bdc733b941a8d40ab08d2384f96ba58f
Eugen Y <johnyjefo@gmail.com>
2022-06-03 09:08:07 +0300

    :rocket: sort trades

--- bot-face/src/App.vue
+++ bot-face/src/App.vue
@@ -4,30 +4,28 @@
     <trading-vue :data="chart" :width="this.width" :height="this.height"
         :chart-config="{MIN_ZOOM:1}"
         ref="tvjs"
-        :toolbar="true"
         :index-based="index_based"
         :overlays="overlays"
     >
     </trading-vue>
-    <span class="gc-mode">
-        <input type="checkbox" v-model="index_based">
-        <label>Index Based</label>
-    </span>
 </span>
 </template>
 
 <script>
 import {TradingVue, DataCube} from 'trading-vue-js'
+import Overlays from 'tvjs-overlays'
+
 import Utils from './stuff/utils.js'
 import Const from './stuff/constants.js'
 // import DataCube from './helpers/datacube.js'
 import Stream from './helpers/stream.js'
+import axios from 'axios'
 // import ScriptOverlay from './Scripts/EMAx6.vue'
 // import BSB from './Scripts/BuySellBalance.vue'
 // Gettin' data through webpeck proxy
 // const PORT = location.port
 const URL = `https://api.binance.com/api/v3/klines?symbol=`
-const WSS = `wss://stream.binance.com:9443/ws/btcusdt@aggTrade`
+const WSS = `wss://stream.binance.com:9443/ws/ltcusdt@aggTrade`
 export default {
     name: 'DataHelper',
     icon: '⚡',
@@ -35,6 +33,33 @@ export default {
     props: ['night'],
     components: {
         TradingVue
+    },
+    created() {
+        axios.get('http://localhost:3000/ltc').then((resp) => {
+            this.performance = resp.data.performance;
+            this.trades = resp.data.trades;
+            let trades = [];
+            this.trades.sort((a, b) => a[0] - b[0]);
+            this.trades.forEach((t) => {
+                trades.push([
+                    t.entry.time,
+                    t.entry.type === 'long' ? 1 : 0,
+                    t.entry.value,
+                    t.entry.type === 'long' ? "L" : "S",
+                ]);
+                trades.push([
+                    t.exit.time,
+                    t.entry.type === 'long' ? 0 : 1,
+                    t.exit.value,
+                    "X",
+                ]);
+            });
+            trades.sort((a, b) => a[0] - b[0]);
+            console.log(trades);
+            // this.chart.set('onchart.TradesPlus.data', trades)
+
+ 
+        });
     },
     mounted() {
         window.addEventListener('resize', this.onResize)
@@ -44,23 +69,51 @@ export default {
         this.load_chunk([now - Const.HOUR4, now]).then(data => {
             this.chart = new DataCube({
                 ohlcv: data['chart.data'],
-                // onchart: [{
-                //     type: 'EMAx6',
-                //     name: 'Multiple EMA',
-                //     data: []
-                // }],
-                // offchart: [{
-                //     type: 'BuySellBalance',
-                //     name: 'Buy/Sell Balance, $lookback',
-                //     data: [],
-                //     settings: {}
-                // }],
+                onchart: [
+                    {
+                        name: 'TradesPlus',
+                        type: 'TradesPlus',
+                        data: [
+                            // [1650123900000, 1, 111.8, "L"],
+                            // [1650261600000, 1, 106.2, "X"],
+                            // [
+                            //     1654155000000,
+                            //     1,
+                            //     63.4,
+                            //     "L"
+                            // ],
+                            // [
+                            //     1654182900154,
+                            //     0,
+                            //     63.5,
+                            //     "Stop"
+                            // ],
+                        ],
+                        settings: {}
+                    }
+                ],
                 datasets: [{
                     type: 'Trades',
                     id: 'binance-btcusdt',
                     data: []
                 }]
             }, { aggregation: 100 })
+            console.log('this.chart', this.chart);
+
+                                    this.chart.set('onchart.TradesPlus.data', 
+                [
+                    1654155000000,
+                    1,
+                    63.4,
+                    "L"
+                ],
+                [
+                    1654182900154,
+                    0,
+                    63.5,
+                    "Stop"
+                ]
+            );
             // Register onrange callback & And a stream of trades
             this.chart.onrange(this.load_chunk)
             this.$refs.tvjs.resetChart()
@@ -79,8 +132,8 @@ export default {
         // use callback: load_chunk(range, tf, callback)
         async load_chunk(range) {
             let [t1, t2] = range
-            let x = 'BTCUSDT'
-            let q = `${x}&interval=1m&startTime=${t1}&endTime=${t2}`
+            let x = 'LTCUSDT'
+            let q = `${x}&interval=15m&startTime=${t1}&endTime=${t2}`
             let r = await fetch(URL + q).then(r => r.json())
             return this.format(this.parse_binance(r))
         },
@@ -97,7 +150,8 @@ export default {
         format(data) {
             // Each query sets data to a corresponding overlay
             return {
-                'chart.data': data
+                'chart.data': data,
+                // 'onchatt'
                 // other onchart/offchart overlays can be added here,
                 // but we are using Script Engine to calculate some:
                 // see EMAx6 & BuySellBalance
@@ -134,9 +188,10 @@ export default {
     data() {
         return {
             chart: {},
+            overlays: [Overlays['TradesPlus']],
             width: window.innerWidth,
             height: window.innerHeight,
-            index_based: false,
+            index_based: true,
             // overlays: [ScriptOverlay, BSB]
         }
     }
